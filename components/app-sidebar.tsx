ead18af01af89cf8e015149ad23b8d63e58979b4
guchimina -desktop <kazu9sum.mi@gmail.com>
2026-02-08 02:19:23 +0900

    メニューアイコン 修正

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -66,7 +66,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                         <SidebarMenuButton size="lg" asChild>
                             <Link href="/" onClick={() => isMobile && setOpenMobile(false)}>
                                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
-                                    <PieChart className="size-4" />
+                                    <img src="/asset-manager/icon.svg" alt="App Logo" className="size-4" />
                                 </div>
                                 <div className="grid flex-1 text-left text-sm leading-tight group-data-[collapsible=icon]:hidden">
                                     <span className="truncate font-semibold uppercase tracking-widest">
